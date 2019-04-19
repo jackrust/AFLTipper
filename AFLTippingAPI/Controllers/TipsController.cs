d446e368dcf186255d86cb3b8cf391ba3578ed51
Jack <jackr@pageuppeople.com>
2019-04-19 18:26:24 +1000

    Removed utilities submodule

--- AFLTippingAPI/Controllers/TipsController.cs
+++ AFLTippingAPI/Controllers/TipsController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Web.Http;
-using AFLStatisticsService;
 using ArtificialNeuralNetwork;
 using AustralianRulesFootball;
 using Tipper.UI;
