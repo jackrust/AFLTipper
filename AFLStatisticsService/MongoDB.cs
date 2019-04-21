8442ced21239e7541c4ca3149a7d98519e3ac5ca
Jack <jackr@pageuppeople.com>
2019-04-21 21:47:45 +1000

    Deleting unused stuff
    Includes currently commented out "Player" code

--- AFLStatisticsService/MongoDB.cs
+++ AFLStatisticsService/MongoDB.cs
@@ -26,37 +26,6 @@ namespace AFLStatisticsService
             DatabaseName = MongoDbConnectionString == LocalConnectionstring ? LocalDatabaseName : RemoteDatabaseName;
         }
 
-        #region Combined
-
-        //TODO: Can we just load player data into season data?
-        /*public List<Season> ReadSeasonAndPlayerDocuments()
-        {
-            var database = LoadMongoDatabase();
-            //Load seasons
-            var seasonsCollection = database.GetCollection<BsonDocument>("season");
-            var seasonsDocument = seasonsCollection.Find(_ => true).ToListAsync();
-            seasonsDocument.Wait();
-            var seasons = ObjectifySeason(seasonsDocument.Result);
-
-            //Load player matches
-            var playersCollection = database.GetCollection<BsonDocument>("player");
-            var playersDocument = playersCollection.Find(_ => true).ToListAsync();
-            playersDocument.Wait();
-            var players = ObjectifySeason(playersDocument.Result);
-            //Append player data
-            foreach (var s in seasons)
-            {
-                foreach (var r in s.Rounds)
-                {
-                    foreach (var m in r.Matches)
-                    {
-                        
-                    }
-                }
-            }
-        } */
-        #endregion
-
         #region ReleaseMongo
         //TODO: Replace List with IEnumerable - it will prevent enumaration early and speed shit up
         public List<Season> ReadSeasonDocument()
@@ -72,8 +41,6 @@ namespace AFLStatisticsService
         //TODO: this is getting silly, better to just create dedicated actions (insert, update, delete) rather than this catch all
         public void UpdateSeasonDocument(List<Season> seasons)
         {
-            //var database = LoadMongoDatabase();
-            //var collection = database.GetCollection<Season>("Seasons");
             var client = new MongoClient(MongoDbConnectionString);
             var session = client.StartSession();
             var databaseSeasons = session.Client.GetDatabase(DatabaseName).GetCollection<Season>("Seasons");
@@ -90,7 +57,6 @@ namespace AFLStatisticsService
                     {
                         var filter = new BsonDocument("Year", s.Year.ToString());
                         databaseSeasons.ReplaceOne(filter, s);
-                        //collection.Update(Query.EQ("Year", s.Year.ToString()), Update.Replace(s), UpdateFlags.Upsert);
                     }
                     else
                     {
@@ -104,153 +70,7 @@ namespace AFLStatisticsService
                 session.AbortTransaction();
                 return;
             }
-
-            var clientz = new MongoClient(MongoDbConnectionString);
-            var sessionz = client.StartSession();
-            var databaseSeasonsz = session.Client.GetDatabase(DatabaseName).GetCollection<Season>("Seasons");
-            var nullFilterz = new FilterDefinitionBuilder<Season>().Empty;
-            var resultsz = databaseSeasons.Find<Season>(nullFilterz).ToList();
         }
-
         #endregion
-
-
-        #region Season
-        /*public void InsertSeasonDocumentz(List<BsonDocument> seasons)
-        {
-            var database = LoadMongoDatabase();
-            var collection = database.GetCollection<BsonDocument>("season");
-
-            var document = new BsonArray();
-            foreach (var season in seasons)
-            {
-                document.Add(season);
-            }
-
-            collection.InsertMany(seasons);
-        }*/
-
-        /*public List<Season> ReadSeasonDocument()
-        {
-            var database = LoadMongoDatabase();
-            var collection = database.GetCollection<BsonDocument>("season");
-            var documents = collection.Find(_ => true).ToListAsync();
-            documents.Wait();
-            return ObjectifySeason(documents.Result);
-        }*/
-        /*
-        public void UpdateSeasonDocument(List<BsonDocument> seasons)
-        {
-            var database = LoadMongoDatabase();
-            var collection = database.GetCollection<BsonDocument>("season");
-
-            foreach (var s in seasons)
-            {
-                var filter = Builders<BsonDocument>.Filter.Eq("year", s.GetValue("year").AsInt32);
-
-                if (collection.Count(filter) > 0)
-                {
-                    collection.ReplaceOne(filter, s);
-                }
-                else
-                {
-                    collection.InsertOne(s);
-                }
-            }
-        }*/
-
-        /*private List<Season> ObjectifySeason(List<BsonDocument> documents)
-        {
-            var seasons = new List<Season>();
-            foreach (var d in documents)
-            {
-                try
-                {
-                    var season = Season.Objectify(d);
-                    seasons.Add(season);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-            }
-            return seasons;
-        }*/
-        #endregion
-
-        #region Player
-        /*public List<Player> ReadPlayerDocument()
-        {
-            var database = LoadMongoDatabase();
-            var collection = database.GetCollection<BsonDocument>("player");
-            var documents = collection.Find(_ => true).ToListAsync();
-            documents.Wait();
-            return ObjectifyPlayer(documents.Result);
-        }
-
-        public void UpdatePlayerDocument(List<BsonDocument> players)
-        {
-            var database = LoadMongoDatabase();
-            var collection = database.GetCollection<BsonDocument>("player");
-
-            foreach (var p in players)
-            {
-                var filter = Builders<BsonDocument>.Filter.Eq("finalSirenPlayerId", p.GetValue("finalSirenPlayerId").AsInt32);
-
-                if (collection.Count(filter) > 0)
-                {
-                    collection.ReplaceOne(filter, p);
-                }
-                else
-                {
-                    collection.InsertOne(p);
-                }
-            }
-        }
-
-        private List<Player> ObjectifyPlayer(List<BsonDocument> documents)
-        {
-            var players = new List<Player>();
-            foreach (var d in documents)
-            {
-                try
-                {
-                    var player = Player.Objectify(d);
-                    players.Add(player);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-            }
-            return players;
-        }*/
-        #endregion
-
-
-        /*private static IMongoDatabase LoadMongoDatabase()
-        {
-            if (!IsProcessOpen("mongod"))
-            {
-                Process.Start(MongoDbExe);
-            }
-
-            var client = new MongoClient("mongodb://localhost:27017");
-            var database = client.GetDatabase(DatabaseName);
-            return database;
-        }*/
-
-        public static bool IsProcessOpen(string name)
-        {
-            foreach (var process in Process.GetProcesses())
-            {
-
-                if (process.ProcessName.Contains(name))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
     }
 }
